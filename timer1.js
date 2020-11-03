7cbf81491d036f168fb46b0ee75924de1c641652
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-03 21:11:11 +0000

    refactored timer1.js

--- timer1.js
+++ timer1.js
@@ -1,9 +1,10 @@
 const args = process.argv.slice(2);
 for (let val of args) {
-  if (!Number.isNaN(Number(val)) && Number(val) >= 0) {
+  val = Number(val);
+  if (!Number.isNaN(val) && val >= 0) {
     setTimeout(() => {
       process.stdout.write('\x07');
       process.stdout.write('.');
-    },Number(val) * 1000);
+    },val * 1000);
   }
 }
